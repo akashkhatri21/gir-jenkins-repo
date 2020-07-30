e5c18af602d0bcef7be0f3a5937d4b5adf17e944
Akash <akashkhatri8269@gmail.com>
2020-07-30 17:30:39 +0530

    updated Welcome file

--- welcome.java
+++ welcome.java
@@ -1,5 +1,9 @@
 public class Welcome{
 	public static void main(String [] args){
 		System.out.println("Welcome to Java Git jenkins Combo");
+		for(int i=0;i<10;i++)
+{
+			System.out.println(i);
+}
 }
 }
